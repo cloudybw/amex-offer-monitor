f7d6a4eecfc7fce211cd1b10a0061f68d2d12316
Bowei Zheng <bzheng@smartlensanalytics.com>
2020-01-27 21:49:17 -0800

    change output path, include google-drive

--- json/parser.py
+++ json/parser.py
@@ -47,14 +47,14 @@ def main():
 		   'Expiration':[],
 		   'Status':[]}
 
-	config_path = os.path.join('/home','cloudybw','study','github','amex-offer-monitor','json','config.json')
+	config_path = os.path.join('/home','cloudybw','google-drive','study','github','amex-offer-monitor','json','config.json')
 	with open(config_path) as configfile:
 		cardmember_dict = json.load(configfile)['cardmembers']
 
 	ending_dict, logins = input(cardmember_dict)
 
 	for login in logins:
-		path = os.path.join('/home','cloudybw','study','github','amex-offer-monitor','json','raw', 'amexoffers-data_' + login + '.json')
+		path = os.path.join('/home','cloudybw','google-drive','study','github','amex-offer-monitor','json','raw', 'amexoffers-data_' + login + '.json')
 
 		try:
 			with open(path) as json_file:
@@ -91,7 +91,7 @@ def main():
 
 	today = date.today().strftime('%Y-%m-%d')
 #	output_path = os.path.join('/Users','bzheng','github','amex-offer-monitor','json','parsed',today+'_summary.csv')
-	output_path = os.path.join('/Users','bzheng','google-drive','study','github','amex-offer-monitor','json','parsed',today+'_summary.csv')
+	output_path = os.path.join('/home','cloudybw','google-drive','study','github','amex-offer-monitor','json','parsed',today+'_summary.csv')
 
 	agg.to_csv(output_path)
 

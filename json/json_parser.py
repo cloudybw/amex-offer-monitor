ba5a1bf1901831a03b5bf34be4aca8f84027f374
Bowei Zheng <bzheng@smartlensanalytics.com>
2020-01-27 21:05:29 -0800

    change back naming convention for gitignore rule

--- json/json_parser.py
+++ json/json_parser.py
@@ -53,7 +53,7 @@ def main():
 	ending_dict, logins = input(cardmember_dict)
 
 	for login in logins:
-		path = os.path.join('raw', login + '.json')
+		path = os.path.join('raw', 'amexoffers-data_' + login + '.json')
 
 		try:
 			with open(path) as json_file:
